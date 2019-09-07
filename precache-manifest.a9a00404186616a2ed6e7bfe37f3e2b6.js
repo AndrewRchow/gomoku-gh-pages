4c4f3ffe9db4aaec79a67c8baf6ce935b9083500
Andrew <chowlick@gmail.com>
2019-09-06 20:20:30 -0700

    Updates

---
+++ precache-manifest.a9a00404186616a2ed6e7bfe37f3e2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "235700e5e0adfb3444dc",
+    "url": "/gomoku-gh-pages/static/js/runtime~main.0890d720.js"
+  },
+  {
+    "revision": "9ca76344ee39ee47f4b2",
+    "url": "/gomoku-gh-pages/static/js/main.b955270f.chunk.js"
+  },
+  {
+    "revision": "ec3ab5c0d846332e008b",
+    "url": "/gomoku-gh-pages/static/js/2.8e81696d.chunk.js"
+  },
+  {
+    "revision": "9ca76344ee39ee47f4b2",
+    "url": "/gomoku-gh-pages/static/css/main.b9c40ada.chunk.css"
+  },
+  {
+    "revision": "ec3ab5c0d846332e008b",
+    "url": "/gomoku-gh-pages/static/css/2.5c03aa34.chunk.css"
+  },
+  {
+    "revision": "4cb387a94a14c59812a94156abc5fdc6",
+    "url": "/gomoku-gh-pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gomoku-gh-pages/precache-manifest.f0bfe28745704261317519e423df8a80.js"
+  "/gomoku-gh-pages/precache-manifest.a9a00404186616a2ed6e7bfe37f3e2b6.js"
 );
 
 workbox.clientsClaim();
